f25a58909efc14db27d859cb4ce9351193766e25
LAPTOP-T58V7O9R\ASUS <thanhlong.nguyen@nfq.asia>
2021-05-04 09:37:38 +0700

    releae APK

--- update.sh
+++ update.sh
@@ -1,3 +1,13 @@
-npm install
+OS="`uname`"
+
+if  [[ $OS == MIN* ]]
+then
+  npm install --no-optional
+else
+  npm install
+fi
+
 rn-nodeify --install --hack
 cp -R ./copy_files/react-native-mqtt/* ./node_modules/react-native-mqtt
+cp -R ./copy_files/react-native-tcp/* ./node_modules/react-native-tcp/android/src/main/java/com/peel/react
+cp -R ./copy_files/react-native-udp/* ./node_modules/react-native-udp/android/src/main/java/com/tradle/react
